e86a61d2034ac1ee1ab121425af571a782217aaf
specella <annnurb@gmail.com>
2019-05-01 17:50:41 -0400

    Primeiro deploy

---
+++ app/controllers/produtos_controller.rb
@@ -0,0 +1,73 @@
+class ProdutosController < ApplicationController
+
+	before_action :set_produto, only: [:edit, :update, :destroy]
+
+	def index
+		@produtos = Produto.order(nome: :asc)
+		@produto_com_desconto = Produto.order(:preco).limit 1
+	end
+
+	def new
+		@produto = Produto.new
+		@departamentos = Departamento.all
+	end
+
+	def create
+		@produto = Produto.new produto_params
+		if @produto.save
+			flash[:notice] = "Produto salvo!"
+			redirect_to root_url
+		else
+			renderiza :new
+		end
+	end
+
+	def edit
+		renderiza :edit
+	end
+
+	def destroy
+		@produto.destroy
+		redirect_to root_url
+	end
+
+	def busca
+		@nome = params[:nome]
+		@produtos = Produto.where "nome like ?", "%#{@nome}%"
+	end
+
+	def update
+		if @produto.update produto_params
+			flash[:notice] = "Produto atualizado com sucesso!"
+			redirect_to root_url
+		else
+			renderiza :edit
+		end
+	end
+
+	private
+
+	def produto_params
+		params.require(:produto).permit(:nome, :descricao, :preco, :quantidade, :departamento_id)
+	end
+
+	def set_produto
+		@produto = Produto.find(params[:id])
+	end
+
+	def renderiza(view)
+		@departamentos = Departamento.all
+		render view
+	end
+end
+
+
+
+
+
+
+
+
+
+
+
